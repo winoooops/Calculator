d4a2cc2ede69a2afa5770c34191c3123b11ca602
weiwang <winoooops@outlook.com>
2020-04-09 16:19:17 +1000

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Will Wong on 2020/4/9.
+//  Copyright © 2020 winoooops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
